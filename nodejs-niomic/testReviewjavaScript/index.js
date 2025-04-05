7ed63a9fa948c5fa3a5c212ef5f49f8a6c2419db
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-05 23:30:23 +0700

    variable

---
+++ nodejs-niomic/testReviewjavaScript/index.js
@@ -0,0 +1,19 @@
+// variable
+
+// var
+var greeter = "Hello World";
+var greeter = "Hallo Dunia"
+console.log(greeter);
+
+// let
+let greeter2 = "Hello World";
+let status = true;
+if (status = true){
+    let greeter2 = "Hallo Dunia";
+}
+console.log(greeter2);
+
+// const
+const greeter3 = "Hello World";
+const greeter3 = "Hallo Dunia";
+console.log(greeter3);
