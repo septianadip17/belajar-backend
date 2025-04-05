b3c670a877a0f6f4ffd12acd82222ad4a81df9b6
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-06 02:27:52 +0700

    mongose done

--- nodejs-niomic/restHub/index.js
+++ nodejs-niomic/restHub/index.js
@@ -1,16 +1,40 @@
+// import express from "express";
 const express = require("express");
+
+// import mongoose
+const mongoose = require("mongoose");
+
+// import body-parser
+const bodyParser = require("express");
+
+// initialize express app
 const app = express();
+
+// setup body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// connect to mongodb
+mongoose.connect("mongodb://localhost:27017/resthub");
+const db = mongoose.connection;
+
+// set port
 const port = 8000;
+
+// import api routes
 const apiRouter = require("./api-routes");
 
+// setup default url
 app.get("/", (req, res) => {
   res.send(
     "Hallo, selamat anda telah berhasil membuat webserver express js dengan nodemon"
-  ); 
+  );
 });
 
-app.use("/api", apiRouter); 
+// api routes ke url /api
+app.use("/api", apiRouter);
 
+// launch app  with info at console log
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
