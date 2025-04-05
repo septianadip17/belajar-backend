53e742b76e7af148df6ac174f875f31e806ac44b
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-05 23:46:47 +0700

    flow perulangan

--- nodejs-niomic/testReviewjavaScript/index.js
+++ nodejs-niomic/testReviewjavaScript/index.js
@@ -17,3 +17,29 @@
 // const greeter3 = "Hello World";
 // const greeter3 = "Hallo Dunia";
 // console.log(greeter3);
+
+
+// perulangan
+
+// // flow for
+// let angka = "";
+// for (let i = 0; i < 10; i++) {
+//     angka = angka + i;
+// }
+// console.log(angka);
+
+// //  flow while
+// let angka = "";
+// while (angka < 10){
+//     angka++
+//     console.log(angka);
+// }
+
+// // flow do while
+// let angka = "";
+// let i = 0;
+// do {
+//   i = i + 1;
+//   angka = angka + i;
+// }while(i < 10);
+// console.log(angka);
