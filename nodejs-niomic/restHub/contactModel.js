59cea7a2468322f37e1a61082d33a7dc6add77f7
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-06 03:10:52 +0700

    take a rest

--- nodejs-niomic/restHub/contactModel.js
+++ nodejs-niomic/restHub/contactModel.js
@@ -1,2 +1,26 @@
 // contactModel.js
 const mongoose = require("mongoose");
+
+// setup schema
+const contactSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  gender: String,
+  phone: Number,
+  create_date: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+// export contact model
+const Contact = module.exports = mongoose.model("contact", contactSchema);
+module.exports.get = function (callback, limit) {
+  Contact.find(callback).limit(limit);
+}
