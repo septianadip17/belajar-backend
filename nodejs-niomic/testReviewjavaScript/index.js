757a179de91be096f131ca1a47e2dd892a54d4ac
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-06 00:01:39 +0700

    switch metode

--- nodejs-niomic/testReviewjavaScript/index.js
+++ nodejs-niomic/testReviewjavaScript/index.js
@@ -1,5 +1,4 @@
-// // variable
-
+// // // variable
 // // var
 // var greeter = "Hello World";
 // var greeter = "Hallo Dunia"
@@ -18,9 +17,7 @@
 // const greeter3 = "Hallo Dunia";
 // console.log(greeter3);
 
-
-// perulangan
-
+// // // perulangan
 // // flow for
 // let angka = "";
 // for (let i = 0; i < 10; i++) {
@@ -43,3 +40,30 @@
 //   angka = angka + i;
 // }while(i < 10);
 // console.log(angka);
+
+// // // function
+// function testNum(a) {
+//   let angka;
+//   if (a > 0) {
+//     angka = "Positif";
+//   } else {
+//     angka = "Negatif";
+//   }
+//   return angka;
+// }
+// console.log(testNum(-5));
+
+const fruit = "Bananas";
+switch (fruit) {
+  case "Papayas":
+    console.log("1kg Papayas = Rp 20.000");
+    break;
+  case "Mangoes":
+    console.log("1kg Mangoes = Rp 15.000");
+    break;
+  case "Bananas":
+    console.log("1kg Bananas = Rp 10.000");
+    break;
+  default:
+    console.log("Sorry, Out Of Order");
+}
