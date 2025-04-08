39fb1e9af7b0da7742c326afbdd0dfe194628cd1
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-09 03:59:38 +0700

    nodemon installed and connection done

--- nodejs-niomic/restApi/server/database/connection.js
+++ nodejs-niomic/restApi/server/database/connection.js
@@ -1,4 +1,24 @@
 // filename: connection.js
 
 // import mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+// connection to mongodb
+const connectDB = async () => {
+  try {
+    // url to database
+    const con = await mongoose.connect("mongodb://localhost:27017/StudyCase", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    // log connection
+    console.log(`MongoDB Connected: ${con.connection.host}`);
+  } catch (err) {
+    // log error
+    console.log(err);
+    // shutdown servers
+    process.exit(1);
+  }
+};
