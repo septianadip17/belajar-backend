25530a953b8635651b9e445b440013a948fa5e60
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-09 23:21:26 +0700

    setup routesusers

--- expressjs-niomic/firstApp/routes/users.js
+++ expressjs-niomic/firstApp/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Login page
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Login Page" });
+});
+
+// Registration page
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "Register Page" });
 });
 
 module.exports = router;
