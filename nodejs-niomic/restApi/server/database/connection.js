5caac4669831d8b19221d9c570a6c30b30443558
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-09 07:44:22 +0700

    there are some function can not supported

--- nodejs-niomic/restApi/server/database/connection.js
+++ nodejs-niomic/restApi/server/database/connection.js
@@ -1,24 +1,46 @@
-// filename: connection.js
+// // filename: connection.js
 
-// import mongoose
-const mongoose = require("mongoose");
+// // import mongoose
+// const mongoose = require("mongoose");
 
-// connection to mongodb
+// // connection to mongodb
+// const connectDB = async () => {
+//   try {
+//     // url to database
+//     const con = await mongoose.connect("mongodb://localhost:27017/StudyCase", {
+//       useUnifiedTopology: true,
+//       useNewUrlParser: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//     });
+//     // log connection
+//     console.log(`MongoDB Connected: ${con.connection.host}`);
+//   } catch (err) {
+//     // log error
+//     console.log(err);
+//     // shutdown servers
+//     process.exit(1);
+//   }
+// };
+
+// // export connection
+// module.exports = connectDB;
+
+const mongoose = require('mongoose');
+
 const connectDB = async () => {
-  try {
-    // url to database
-    const con = await mongoose.connect("mongodb://localhost:27017/StudyCase", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
-    // log connection
-    console.log(`MongoDB Connected: ${con.connection.host}`);
-  } catch (err) {
-    // log error
-    console.log(err);
-    // shutdown servers
-    process.exit(1);
-  }
-};
+    try {
+        // mongodb connection string
+        const con = await mongoose.connect('mongodb://localhost:27017/StudyCase', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        console.log(`MongoDB connected: ${con.connection.host}`);
+    } catch(err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- nodejs-niomic/restApi/server/routes/router.js
+++ nodejs-niomic/restApi/server/routes/router.js
@@ -4,10 +4,13 @@
 const express = require("express");
 const route = express.Router();
 
-const controller = require("../controllers/controller");
+const controller = require("../controller/controller");
 
 // setup endpoint url request
 route.post("/api/users", controller.create);
 route.get("/api/users", controller.find);
 route.put("/api/users/:username", controller.update);
 route.delete("/api/users/:username", controller.delete);
+
+// export router
+module.exports = route;
