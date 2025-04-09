d47313d0a98869e252ef10f1ff48fe9f01a85845
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-09 23:15:39 +0700

    setup route index

--- expressjs-niomic/firstApp/routes/index.js
+++ expressjs-niomic/firstApp/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// welcome page
+router.get("/", function (req, res, next) {
+  res.render("welcome", { title: "Welcome Page" });
 });
 
+// dashboard page
+router.get("/dashboard", function (req, res, next) {
+  res.render("dashboard", { title: "Dashboard Page" });
+});
 module.exports = router;
