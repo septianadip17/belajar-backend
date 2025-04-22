3868a96d1500739b71998c2e4f4ab57acd698479
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-22 20:59:25 +0700

    add added

--- nodejs-udemy/express-server/index.js
+++ nodejs-udemy/express-server/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 3000;
 
@@ -6,6 +6,19 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/add", (req, res) => {
+  const nums = req.query.nums;
+  if (!nums) {
+    return res.send("nums is required");
+  }
+  const numbers = nums.split(",");
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += parseInt(numbers[i]);
+  }
+  res.send(sum.toString());
+});
