93ea181e9367e89c401ce8d547b4154da899d6f8
Septian Adi Pratama <septianadip7998@gmail.com>
2025-04-27 22:24:39 +0700

    get request

--- nodejs-udemy/http-request/index.js
+++ nodejs-udemy/http-request/index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! hehe");
+
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
