9e04f063ac780cf1e5585b9fbf954758691ffec9
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-01 16:50:41 +0700

    contact added

--- nodejs-udemy/http-request/index.js
+++ nodejs-udemy/http-request/index.js
@@ -4,12 +4,15 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
-  console.log(req.rawHeaders);
 });
 
 app.get("/about", (req, res) => {
   res.send("<h1>About Me</h1>");
-  console.log(req.rawHeaders);
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Me</h1><p>HP: +62.......</p>");
+
 });
 
 app.listen(port, () => {
