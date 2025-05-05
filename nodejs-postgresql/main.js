ae796e6e7d9a856a5e132319c4d075059ab5131c
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-05 10:42:31 +0700

    data inserted successfully

--- nodejs-postgresql/main.js
+++ nodejs-postgresql/main.js
@@ -1,7 +1,8 @@
 const { Client } = require("pg");
 const express = require("express");
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
+
 const con = new Client({
   host: "localhost",
   user: "postgres",
@@ -10,4 +11,22 @@ const con = new Client({
   database: "demopost",
 });
 
+app.post("/postData", (req, res) => {
+  const { name, id } = req.body;
+  const insert_query = "INSERT INTO demotable (name, id) VALUES ($1, $2)";
+
+  con.query(insert_query, [name, id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("error");
+    } else {
+      console.log(result);
+      res.send("data inserted");
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is running...");
+});
 con.connect().then(() => console.log("connected"));
