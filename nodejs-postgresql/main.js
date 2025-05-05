540fedb9d66f43086b086d37420c0de256181f79
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-05 09:42:07 +0700

    solved password

--- nodejs-postgresql/main.js
+++ nodejs-postgresql/main.js
@@ -1,1 +1,11 @@
-const {Client} = require('pg');
+const { Client } = require("pg");
+
+const con = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "root123",
+  database: "demopost",
+});
+
+con.connect().then(() => console.log("connected"));
