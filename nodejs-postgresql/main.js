1be55e291db47c8404bc2da83ce03d8aabb9bf75
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-05 11:47:28 +0700

    successfully get data by id

--- nodejs-postgresql/main.js
+++ nodejs-postgresql/main.js
@@ -14,7 +14,6 @@ const con = new Client({
 app.post("/postData", (req, res) => {
   const { name, id } = req.body;
   const insert_query = "INSERT INTO demotable (name, id) VALUES ($1, $2)";
-
   con.query(insert_query, [name, id], (err, result) => {
     if (err) {
       console.log(err);
@@ -25,6 +24,33 @@ app.post("/postData", (req, res) => {
     }
   });
 });
+
+app.get("/fetchData", (req, res) => {
+  const fetch_query = "SELECT * FROM demotable";
+  con.query(fetch_query, (err, result) => {
+    if (err) {
+      // console.log(err);
+      res.send("error");
+    } else {
+      // console.log(result);
+      res.send(result.rows);
+    }
+  });
+});
+
+app.get("/fetchById/:id", (req, res) => {
+  const { id } = req.params;
+  const fetch_query = "SELECT * FROM demotable WHERE id = $1";
+  con.query(fetch_query, [id], (err, result) => {
+    if (err) {
+      // console.log(err);
+      res.send("error");
+    } else {
+      console.log(result.rows);
+      res.send(result.rows[0]);
+    }
+  })
+})
 
 app.listen(3000, () => {
   console.log("server is running...");
