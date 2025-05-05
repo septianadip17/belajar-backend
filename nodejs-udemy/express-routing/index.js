d0a35b69810b2113bfdf6ba3ce23dd6aa4a247b9
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-05 08:25:18 +0700

    wildcard still error

--- nodejs-udemy/express-routing/index.js
+++ nodejs-udemy/express-routing/index.js
@@ -1,20 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/cats', (req, res) => {
-  res.send('Meow!'); 
-})
+app.get("/cats", (req, res) => {
+  res.send("Meow!");
+});
 
-app.get('/dogs', (req, res) => {
-  res.send('Woof!');
-  console.log('Someone made a request to /dogs');
-})
+app.post("/cats", (req, res) => {
+  res.send("Post request to the homepage");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("Woof!");
+  console.log("Someone made a request to /dogs");
+});
+
+app.get("*", (req, res) => {
+  console.log(req.path);
+  res.send("I dont know that path!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
