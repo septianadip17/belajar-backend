99f55d8f8f01a2008ddca894e2896fb9ce345819
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-05 12:26:34 +0700

    address added column data

--- nodejs-postgresql/main.js
+++ nodejs-postgresql/main.js
@@ -17,7 +17,7 @@ app.post("/postData", (req, res) => {
   const insert_query = "INSERT INTO demotable (name, id) VALUES ($1, $2)";
   con.query(insert_query, [name, id], (err, result) => {
     if (err) {
-      console.log(err);
+      // console.log(err);
       res.send("error");
     } else {
       console.log(result);
@@ -49,8 +49,24 @@ app.get("/fetchById/:id", (req, res) => {
       // console.log(err);
       res.send("error");
     } else {
-      console.log(result.rows);
+      // console.log(result.rows);
       res.send(result.rows[0]);
+    }
+  })
+})
+
+// update data by id
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, address } = req.body;
+  const update_query = "UPDATE demotable SET name = $1, address = $2 WHERE id = $3";
+  con.query(update_query, [name, address, id], (err, result) => {
+    if (err) {
+      // console.log(err);
+      res.send("error");
+    } else {
+      // console.log(result);
+      res.send("data updated");
     }
   })
 })
