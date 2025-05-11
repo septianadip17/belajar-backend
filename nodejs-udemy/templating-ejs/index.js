52a338dec91e602a2edd68638546e0ac9da41a3c
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-11 23:05:29 +0700

    successfully showed home

--- nodejs-udemy/templating-ejs/index.js
+++ nodejs-udemy/templating-ejs/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home.ejs')
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
