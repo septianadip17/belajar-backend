d12707f2f03e9e5dbe1fbc96e17a4c8b474e6f1e
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-11 23:13:55 +0700

    sucessfully running from the outside folder

--- nodejs-udemy/templating-ejs/index.js
+++ nodejs-udemy/templating-ejs/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home.ejs')
