e0495fe6f59b69e60733c86bf8f9602f82728114
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-11 23:34:35 +0700

    passing ejs

--- nodejs-udemy/templating-ejs/index.js
+++ nodejs-udemy/templating-ejs/index.js
@@ -1,15 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path');
+const path = require("path");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-  res.render('home.ejs')
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { num });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
